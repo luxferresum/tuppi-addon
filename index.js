f00a4cf010f3474003cf6e5ffbb21d20aa0db4fc
Lukas Kohler <lukas.kohler@ontheblueplanet.com>
2018-07-15 21:19:52 +0200

    include TUPPI_TEMPLATE_DIR

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 'use strict';
 
+const Funnel = require('broccoli-funnel');
+const MergeTrees = require('broccoli-merge-trees');
+const TemplateParser = require('./template-parser');
+
 module.exports = {
-  name: 'tuppi-addon'
+  name: 'tuppi-addon',
+  treeForAddon() {
+    var tree = this._super.treeForAddon.apply(this, arguments);
+    console.log('env' + process.env.TUPPI_TEMPLATE_DIR);
+
+    const template = new Funnel(process.env.TUPPI_TEMPLATE_DIR);
+    const processed = new TemplateParser([template]);
+    return new MergeTrees([tree, processed]);
+  },
+
+  isDevelopingAddon() {
+    return true;
+  },
 };
---
+++ template-parser.js
@@ -0,0 +1,42 @@
+const Plugin = require('broccoli-plugin');
+const fs = require('fs');
+const path = require('path');
+
+// Create a subclass TemplateParser derived from Plugin
+TemplateParser.prototype = Object.create(Plugin.prototype);
+TemplateParser.prototype.constructor = TemplateParser;
+function TemplateParser(inputNodes, options) {
+  options = options || {};
+  Plugin.call(this, inputNodes, {
+    annotation: options.annotation
+  });
+  this.options = options;
+}
+
+TemplateParser.prototype.build = function() {
+  // Read files from this.inputPaths, and write files to this.outputPath.
+  // Silly example:
+
+  const data = this.inputPaths
+    .map(p => fs.readdirSync(p).map(f => path.join(p, f)))
+    .reduce((a, b) => [...a, ...b], [])
+    .map(f => ({
+      name: path.basename(f, path.extname(f)),
+      extension: path.extname(f),
+      content: fs.readFileSync(f, { encoding: 'utf8' }),
+    }))
+    .reduce((arr, elem) => {
+      arr.push(elem)
+      return arr;
+    }, []);
+
+  const content = `
+    define('tuppi-addon', [], function() {
+      return ${JSON.stringify(data)};
+     });
+  `;
+
+  fs.writeFileSync(path.join(this.outputPath, 'content.js'), content, { encoding: 'utf8' });
+};
+
+module.exports = TemplateParser;
