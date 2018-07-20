6fb39add332f143244193bdf354191ca702ea6b5
Lukas Kohler <lukas.kohler@ontheblueplanet.com>
2018-07-20 19:48:57 +0200

    test

--- tests/unit/services/tuppi-test.js
+++ tests/unit/services/tuppi-test.js
@@ -4,10 +4,15 @@ import { setupTest } from 'ember-qunit';
 module('Unit | Service | tuppi', function(hooks) {
   setupTest(hooks);
 
+  hooks.beforeEach(function() {
+    this.foo = 12;
+  });
+
   // Replace this with your real tests.
   test('it exists', function(assert) {
     let service = this.owner.lookup('service:tuppi');
     assert.ok(service);
+    assert.equal(this.foo, 13);
   });
 });
 
